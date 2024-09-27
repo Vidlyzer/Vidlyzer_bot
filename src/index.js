5ecc4e42817840fa449bbdd25b7ad21bc76d2611
rax <>
2024-09-27 11:54:24 +0700

    main

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+require('dotenv').config();
+
 
 client.once('ready', () => {
     console.log('Bot is online!');
 });
 
-client.login('TOKEN_DISCORD_BOT_KAMU');
+client.login('process.env.TOKEN');
